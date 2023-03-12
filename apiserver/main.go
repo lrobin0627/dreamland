fa3e902e2715d3780c2c9b66d9e3d977372873a0
lrobin0627 <lipf160627@163.com>
2023-03-12 22:04:00 +0800

    Add actions and apiserver

---
+++ apiserver/main.go
@@ -0,0 +1,19 @@
+// Copyright 2023 Leo <lipf160627@163.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/hello", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World Golang")
+	})
+	e.Logger.Fatal(e.Start(":8080"))
+}
